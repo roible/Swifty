5fd1de62cdc9acba17ea4c30d2f9de732be4d017
roy <royite.ren@gmail.com>
2019-03-26 23:00:38 +0800

    Modify CellRegisterable

--- Sources/Swifty/Core/CellRegisterable.swift
+++ Sources/Swifty/Core/CellRegisterable.swift
@@ -25,29 +25,43 @@ extension UICollectionView: SwiftyCompatible {}
 
 extension SwiftyWrapper where Base: UITableView {
     @discardableResult
-    public func register<Cell: CellRegisterable>(cellClass: Cell.Type) -> Base {
+    public func register<Cell>(cellClass: Cell.Type) -> Base where Cell: CellRegisterable {
         base.register(Cell.self, forCellReuseIdentifier: cellClass.identifier)
         return base
     }
     
     @discardableResult
-    public func register<Cell: NibCellRegisterable>(nibCellClass: Cell.Type) -> Base {
+    public func register<Cell>(nibCellClass: Cell.Type) -> Base where Cell: NibCellRegisterable {
         base.register(nibCellClass.nib, forCellReuseIdentifier: nibCellClass.identifier)
         return base
+    }
+    
+    public func dequeueReusableCell<Cell>(of cellType: Cell.Type,
+                                          for indexPath: IndexPath) -> Cell where Cell: CellRegisterable {
+        return base.dequeueReusableCell(withIdentifier: Cell.identifier, for: indexPath) as! Cell
+    }
+    
+    public func dequeueReusableCell<Cell>(of cellType: Cell.Type) -> Cell? where Cell: CellRegisterable {
+        return base.dequeueReusableCell(withIdentifier: Cell.identifier) as Cell
     }
 }
 
 extension SwiftyWrapper where Base: UICollectionView {
     @discardableResult
-    public func register<Cell: CellRegisterable>(cellClass: Cell.Type) -> Base {
+    public func register<Cell>(cellClass: Cell.Type) -> Base where Cell: CellRegisterable {
         base.register(Cell.self, forCellWithReuseIdentifier: cellClass.identifier)
         return base
     }
     
     @discardableResult
-    public func register<Cell: NibCellRegisterable>(nibCellClass: Cell.Type) -> Base {
+    public func register<Cell>(nibCellClass: Cell.Type) -> Base where Cell: NibCellRegisterable {
         base.register(nibCellClass.nib, forCellWithReuseIdentifier: nibCellClass.identifier)
         return base
+    }
+    
+    public func dequeueReusableCell<Cell>(of cellType: Cell.Type,
+                                          for indexPath: IndexPath) -> Cell where Cell: CellRegisterable {
+        return base.dequeueReusableCell(withReuseIdentifier: cellType.identifier, for: indexPath) as! Cell
     }
 }
 #endif
