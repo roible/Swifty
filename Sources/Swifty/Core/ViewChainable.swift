40bb4d15b821045c379d0ff2bb63c492a6950761
Roy <royite.ren@gmail.com>
2019-04-09 22:50:30 +0800

    Modify ViewChainable

--- Sources/Swifty/Core/ViewChainable.swift
+++ Sources/Swifty/Core/ViewChainable.swift
@@ -7,25 +7,25 @@
 
 #if os(iOS)
 import UIKit
-typealias View = UIView
+public typealias View = UIView
 #elseif os(macOS)
 import AppKit
-typealias View = NSView
+public typealias View = NSView
 #endif
 
 import Foundation
 
-protocol ViewChainable {}
+public protocol ViewChainable {}
 
 extension ViewChainable where Self: View {
     @discardableResult
-    func added(to superView: View) -> Self {
+    public func added(to superView: View) -> Self {
         superView.addSubview(self)
         return self
     }
     
     @discardableResult
-    func config(_ configClosure: (Self) -> Void) -> Self {
+    public func config(_ configClosure: (Self) -> Void) -> Self {
         configClosure(self)
         return self
     }
